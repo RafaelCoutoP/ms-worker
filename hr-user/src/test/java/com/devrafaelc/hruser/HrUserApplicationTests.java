b7ef9b087551ba56cdf1b2f44ba6f936414377b3
rafaelc <rafaelcouto@hotmail.com>
2020-12-08 20:07:48 -0300

    Projec hr-user created

---
+++ hr-user/src/test/java/com/devrafaelc/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrafaelc.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
